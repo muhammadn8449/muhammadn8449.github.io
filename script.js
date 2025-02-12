4c2bf1f00281067df6d81e9b6c3fd07c02fa7624
muhammadn8449 <MUhammadkhonn@nycstudents.net>
2025-02-12 17:06:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ownerName = "Muhammad N"; // update the part BETWEEN the "quotes"
+let ownerName = "Muhammad N."; // update the part BETWEEN the "quotes"
 let userName = "muhammadn8449"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
